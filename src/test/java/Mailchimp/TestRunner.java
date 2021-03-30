8ff7ce1a157a4a590bae2cec129073e7c6a865a3
Jennie <jennie.lifvendahl@live.se>
2021-03-30 17:42:28 +0200

    All four tests now written.

--- src/test/java/Mailchimp/TestRunner.java
+++ src/test/java/Mailchimp/TestRunner.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features", glue= "Mailchimp", plugin={"pretty"})
+@CucumberOptions(features="src/test/resources/Features", glue= "Mailchimp", plugin={"pretty"}/*, tags="@takenUsername"*/)
 public class TestRunner {
 }
